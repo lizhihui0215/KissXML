c5f0831b18458e25fe947abd534ad95393d28a76
李智慧 <lizhihui0215@gmail.com>
2017-01-07 13:23:41 +0800

    add carthage support

---
+++ KissXML/KissXML.h
@@ -0,0 +1,19 @@
+//
+//  KissXML.h
+//  KissXML
+//
+//  Created by 李智慧 on 2017/1/7.
+//  Copyright © 2017年 李智慧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KissXML.
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+
+//! Project version string for KissXML.
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
+
+
